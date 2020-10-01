0f46dab5be8a3d89988e74de0d141186d7394842
maxcramer <maximiliancramer@yahoo.com>
2020-10-01 16:15:39 +0100

    adding styling to nav bar and changing names on navbar items

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import HeadBanner from './components/HeadBanner/HeadBanner'
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      {/* <header className="App-header">
+      </header> */}
+      <NavBar />
       <HeadBanner />
     </div>
   );
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './NavBar.scss';
+
+function NavBar() {
+
+    return (
+        <div className="navbar">
+            <h4>Personal Projects</h4>
+            <h4>Development Work</h4>
+            <h4>Development Positions</h4>
+            <h4>About Max</h4>
+            <h4>Onewheel</h4>
+            <h4>Links</h4>
+            <h4>Contact</h4>
+        </div>
+    );
+}
+
+export default NavBar;
