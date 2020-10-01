fd002dde092df0b8828957101b5d1a13a1116627
maxcramer <maximiliancramer@yahoo.com>
2020-10-01 22:31:34 +0100

    adding logo for github next to github title

--- src/components/GitHubGraph/GitHubGraph.jsx
+++ src/components/GitHubGraph/GitHubGraph.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
+import githubLogo from '../media/githubLogo.png';
+
 import './GitHubGraph.scss';
 
 function GitHubGraph() {
     return (
         <div className="graph_container">
-            <h2 id="graph_title">GitHub Commits</h2>
-            <img className="graph_image" src="https://grass-graph.moshimo.works/images/maxcramer.png" />
+            <span className="graph_title_and_logo">
+                <h2 id="graph_title">GitHub Commits</h2>
+                <img id="github_logo" src={githubLogo} alt=""/>
+            </span>
+                <img className="graph_image" src="https://grass-graph.moshimo.works/images/maxcramer.png?background=none" />
         </div>
     )
-}
+} 
 
 export default GitHubGraph;
