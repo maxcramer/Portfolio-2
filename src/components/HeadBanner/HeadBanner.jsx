bf67b586b123fa2db49e3e42ae6852bc5ebe1ce5
maxcramer <maximiliancramer@yahoo.com>
2020-10-01 13:43:45 +0100

    header banner divs complete, just need to adjust text in this section

--- src/components/HeadBanner/HeadBanner.jsx
+++ src/components/HeadBanner/HeadBanner.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import MyMovie from '../media/MyMovie.mp4';
+import './HeadBanner.scss';
 
 
 function HeadBanner () {
@@ -8,9 +9,18 @@ function HeadBanner () {
 
     return (
         <div>
+            <div className="full_opaque_background">
+            </div>
+            <div className="side_background">
+                <div className="text_items">
+                <h1>Max Cramer</h1>
+                <h3>Web Developer</h3>
+                <h3>Pro Onewheel Rider</h3>
+                </div>
+            </div>
             <video id="header_video" autoPlay loop muted >
-                <source src={MyMovie} type="video/mp4" />
-            </video>
+                    <source src={MyMovie} type="video/mp4" />
+                </video>
         </div>
         
     )
