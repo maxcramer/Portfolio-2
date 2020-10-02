b9ec0b443899f5e2f83003bfec6dd43e52bdeca5
maxcramer <maximiliancramer@yahoo.com>
2020-10-02 10:56:49 +0100

    added inner container to graph component

--- src/components/GitHubGraph/GitHubGraph.jsx
+++ src/components/GitHubGraph/GitHubGraph.jsx
@@ -7,11 +7,14 @@ import './GitHubGraph.scss';
 function GitHubGraph() {
     return (
         <div className="graph_container">
+            <div id="graph_border">
             <span className="graph_title_and_logo">
                 <h2 id="graph_title">GitHub Commits</h2>
                 <img id="github_logo" src={githubLogo} alt=""/>
             </span>
                 <img className="graph_image" src="https://grass-graph.moshimo.works/images/maxcramer.png?background=none" />
+            </div>
+            
         </div>
     )
 } 
