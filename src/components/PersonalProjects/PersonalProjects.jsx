9b5f85164da3ca0d05b345bd59a1ee40f02ad0b6
maxcramer <maximiliancramer@yahoo.com>
2020-10-02 16:14:49 +0100

    personal project titles rendering in map function, data coming from firebase cloud firestore

--- src/components/PersonalProjects/PersonalProjects.jsx
+++ src/components/PersonalProjects/PersonalProjects.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { getPersonalProjects } from '../../services/firestoreService';
 
 import './PersonalProjects.scss';
 
 function PersonalProjects() {
+    const [personalProjects, setPersonalProjects] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const personalProjects = await getPersonalProjects();
+            setPersonalProjects(personalProjects);
+        }
+        fetchData();
+    }, [])
     return (
         <div className="personalProjects_container">
-            <h3>Personal Projects</h3>
+            <ul>
+                {personalProjects.map(p => (
+                    <div key={p._id}>
+                        <li>
+                            <h3>
+                                {p.title}
+                            </h3>
+                        </li>
+                    </div>
+                ))}
+            </ul>
         </div>
     )
 }
