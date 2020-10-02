5c73d272ccb66eb14bbf3cf765d25ebb87764249
maxcramer <maximiliancramer@yahoo.com>
2020-10-02 12:14:53 +0100

    adding changes to graph

---
+++ src/services/firestoreService.js
@@ -0,0 +1,18 @@
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/functions";
+import "firebase/auth";
+
+const firebaseConfig = {
+apiKey: "AIzaSyC_wuXbHUt36ltVYsSM95gckKKN8vHT2P0",
+authDomain: "funkynana-bf9f1.firebaseapp.com",
+databaseURL: "https://funkynana-bf9f1.firebaseio.com",
+projectId: "funkynana-bf9f1",
+storageBucket: "funkynana-bf9f1.appspot.com",
+messagingSenderId: "66009035565",
+appId: "1:66009035565:web:afeba383873790307659a4",
+measurementId: "G-5FBD780DY7"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
