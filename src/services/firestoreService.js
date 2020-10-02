e70b8cc4bca8a9b2e41a4178d1027db7fad753ac
maxcramer <maximiliancramer@yahoo.com>
2020-10-02 12:51:32 +0100

    updates for firebase

--- src/services/firestoreService.js
+++ src/services/firestoreService.js
@@ -3,16 +3,16 @@ import "firebase/firestore";
 import "firebase/functions";
 import "firebase/auth";
 
-const firebaseConfig = {
-apiKey: "AIzaSyC_wuXbHUt36ltVYsSM95gckKKN8vHT2P0",
-authDomain: "funkynana-bf9f1.firebaseapp.com",
-databaseURL: "https://funkynana-bf9f1.firebaseio.com",
-projectId: "funkynana-bf9f1",
-storageBucket: "funkynana-bf9f1.appspot.com",
-messagingSenderId: "66009035565",
-appId: "1:66009035565:web:afeba383873790307659a4",
-measurementId: "G-5FBD780DY7"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+var firebaseConfig = {
+    apiKey: "AIzaSyC_wuXbHUt36ltVYsSM95gckKKN8vHT2P0",
+    authDomain: "funkynana-bf9f1.firebaseapp.com",
+    databaseURL: "https://funkynana-bf9f1.firebaseio.com",
+    projectId: "funkynana-bf9f1",
+    storageBucket: "funkynana-bf9f1.appspot.com",
+    messagingSenderId: "66009035565",
+    appId: "1:66009035565:web:afeba383873790307659a4",
+    measurementId: "G-5FBD780DY7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
