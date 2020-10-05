bb014882b27032fe8c603430280ec839b78725db
maxcramer <maximiliancramer@yahoo.com>
2020-10-05 16:40:49 +0100

    creating new component work

---
+++ src/components/Work/Work.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+import './Work.scss';
+
+
+function Work() {
+
+    
+    return (
+        <div className="work_container">
+            <h1 className="work_title">
+                Development Work During Employment 
+            </h1>
+        </div>
+    )
+}
+
+
+export default Work;
