a2f66e5862a02d6be7de319468eb15cf04a9dda4
maxcramer <maximiliancramer@yahoo.com>
2020-10-07 17:26:29 +0100

    updaing styling in Positions component

--- src/components/Positions/Positions.jsx
+++ src/components/Positions/Positions.jsx
@@ -26,9 +26,29 @@ function Positions() {
                    <div key={p._id}>
                        <li>
                            <img src={p.Logo} alt=""/>
-                           <div className="company_position">
-                                <h3>Company: {p.Company}</h3>
-                                <h3>Position: {p.Position}</h3>
+                           <div className="info">
+                                <div className="company_position">
+                                    <h3>Company: {p.Company}</h3>
+                                    <h3>Position: {p.Position}</h3>
+                                </div>
+                                <div className="dates">
+                                    <p>
+                                        {p.StartDate} - {p.EndDate}
+                                    </p>
+                                    |
+                                    <p>Duration: {p.Duration}</p>
+                                </div>
+                                <div className="proj_tech_sal">
+                                    <p>
+                                        Projects: {p.Projects}
+                                    </p>
+                                    <p>
+                                        Technology: {p.TechUsed}
+                                    </p>
+                                    <p>
+                                        Salary: {p.Salary}                                    
+                                    </p>
+                                </div>
                            </div>
                        </li>
                    </div>
