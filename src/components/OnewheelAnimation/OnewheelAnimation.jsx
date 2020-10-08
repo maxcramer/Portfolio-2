29401c90729d59753614c078f2a57be86d35c460
maxcramer <maximiliancramer@yahoo.com>
2020-10-08 11:55:22 +0100

    adding Onewheel Animation component

---
+++ src/components/OnewheelAnimation/OnewheelAnimation.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import './OnewheelAnimations.scss';
+
+
+function OnewheelAnimations() {
+
+    return (
+        <div className="rider">
+            {/* This runs in annimation, check CSS file */}
+            <p> 
+                ⏜__
+            </p>
+            <p>
+                (⌇•_)
+            </p>
+            <p>
+                /|\
+            </p>
+            <p>
+                / | \
+            </p>
+            <p>
+                / \
+            </p>
+            <p>
+                I  I    
+            </p> 
+            <p>
+                ---o---
+            </p>
+        </div>
+    )
+}
+
+export default OnewheelAnimations;
