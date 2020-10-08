004c0ac15e50606bf88b3977356dfd6c0c6cd05d
maxcramer <maximiliancramer@yahoo.com>
2020-10-08 17:45:40 +0100

    adding onewheel component

---
+++ src/components/Onewheel/Onewheel.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import InstagramEmbed from 'react-instagram-embed';
+
+
+import './Onewheel.scss';
+
+function Onewheel() {
+
+    return (
+        <div id="onewheel">
+            <div id="onewheel_title_div">
+                <h1>Onewheel</h1>
+            </div>
+            <div id="onewheel_about_insta">
+                <div id="about_onewheel">
+                    <p>
+                        info about me riding onewheel
+                    </p>
+                </div>
+                <div id="instafeed">
+                    <h3>Instagram Feed?</h3>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Onewheel;
