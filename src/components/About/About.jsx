64e36a5fbc676e8d8fbec3157793b652dc7bad54
maxcramer <maximiliancramer@yahoo.com>
2020-10-08 14:17:56 +0100

    styled out and added About paragraph

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,10 +6,14 @@ function About() {
     return (
         <div id="about_container">
             <h3 id="about_title">About Max</h3>
-            <p>
-            Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.
-
-
+            <p id="about_desc">
+            Previous to the pandemic I had been working at Cobidol, as a Junior Full Stack Developer. The entire system was built using AngularJs, which gave me a great opportunity to practice and learn more using Angular. My first project at Cobidol was to build a CACLTV Calculator, which I used Angular Materials for components and styling. After that, I was mainly working on building out reducers, effects, and selectors within the actual project while using Observables and saving data to the Store. I also had an opportunity to work on some tests using Jest Marble. These tests would allow us to create objects and check whether they had the correct properties attached to them.
+            <br />
+            My interest in web development and writing code was perked, as previously I had attempted to launch a tech startup doing vegan & vegetarian delivery service.
+            Having grown up enjoying building websites using online templates, being fascinated by both the programing behind it as well as a love for art and creativity, I decided to grow my skills in web development at General Assembly.
+            <br />
+            Web development as a career excites me due to the scope of ability to move up within an organization. I also firmly believe that being able to learn and further those skills while working is a huge bonus that I have not seen in many industries before.
+            Ideally, I would be working within a close-knit, friendly development team, which would allow me to grow my skills over a long term basis. My dream company would be an ethical one that would help increase people's standard of living or health.
             </p>
         </div>
     )
