b2847e3774439072b14074813c235c30315f08b8
maxcramer <maximiliancramer@yahoo.com>
2020-10-12 16:42:28 +0100

    building out nav bar for mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,19 +2,38 @@ import React from 'react';
 
 import './NavBar.scss';
 
-function NavBar() {
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 
-    return (
-        <div className="navbar">
-            <h4><a href="#personalprojects">Personal Projects</a></h4>
-            <h4><a href="#work">Development Work</a></h4>
-            <h4><a href="#position">Development Positions</a></h4>
-            <h4><a href="#about_container">About Max</a></h4>
-            <h4><a href="#onewheel">Onewheel</a></h4>
-            <h4><a href="#links">Links</a></h4>
-            <h4><a href="#contacts">Contact</a></h4>
-        </div>
-    );
-}
+
+const NavBar = props => (
+    <header className="navbar">
+        <nav>
+            <div>
+                <DrawerToggleButton click={props.drawerClickHandler}/>
+            </div>
+            <div className="navbar_desktop">
+                <h4><a href="#personalprojects">Personal Projects</a></h4>
+                <h4><a href="#work">Development Work</a></h4>
+                <h4><a href="#position">Development Positions</a></h4>
+                <h4><a href="#about_container">About Max</a></h4>
+                <h4><a href="#onewheel">Onewheel</a></h4>
+                <h4><a href="#links">Links</a></h4>
+                <h4><a href="#contacts">Contact</a></h4>
+            </div>
+        </nav>
+    </header>
+)
 
 export default NavBar;
+
+
+// function NavBar() {
+
+//     return (
+//         <div className="navbar">
+//           
+//         </div>
+//     );
+// }
+
+// export default NavBar;
