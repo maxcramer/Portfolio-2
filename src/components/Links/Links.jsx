e55b1e2a3450f64747245f348009aba04e344e44
maxcramer <maximiliancramer@yahoo.com>
2020-10-12 14:41:45 +0100

    creating and setting up basics for Links component

---
+++ src/components/Links/Links.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Links.scss';
+
+function Links() {
+    return (
+        <div id="links">
+            <h1>Links</h1>
+        </div>
+    )
+}
+
+export default Links;
