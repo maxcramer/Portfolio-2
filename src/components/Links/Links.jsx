de860ba88ad4498ac0b8923261fc758cc337b028
maxcramer <maximiliancramer@yahoo.com>
2020-10-12 15:05:52 +0100

    built and styled links component for desktop

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
+import githubLogo from '../media/githubLogo.png'
+
 import './Links.scss';
 
 function Links() {
     return (
         <div id="links">
             <h1>Links</h1>
+            <div id="links_images">
+                {/* GITHUB */}
+                <a href="https://github.com/maxcramer">
+                    <img id="github" src={githubLogo} alt=""/>
+                </a>
+                {/* LINKEDIN */}
+                <a href="https://www.linkedin.com/in/max-cramer/">
+                    <img id="linkedIn" src="https://image.flaticon.com/icons/png/512/174/174857.png" alt=""/>
+                </a>
+            </div>
         </div>
     )
 }
