609dd943e42f5670481d54f9c3759a960122955e
maxcramer <maximiliancramer@yahoo.com>
2020-10-12 15:21:35 +0100

    created and styled contact component for desktop

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Contact.scss';
+
+function Contact() {
+    return (
+        <div id="contacts">
+            <h1>
+                Contact
+            </h1>
+            <div>
+                <p id="email">Please reach out at <a  href = "mailto:info@maxcramer101.com?subject = Feedback&body = Message">info@maxcramer101.com</a></p>
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
