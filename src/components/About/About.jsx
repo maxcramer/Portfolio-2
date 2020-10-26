e9829a8261387be464dbd2d6740770c5a7fc0f3d
maxcramer <maximiliancramer@yahoo.com>
2020-10-26 11:34:27 +0000

    updating about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,16 +7,17 @@ function About() {
         <div id="about_container">
             <h3 id="about_title">About Max</h3>
             <p id="about_desc">
-            Previous to the pandemic I had been working at Cobidol, as a Junior Full Stack Developer. The entire system was built using AngularJs, which gave me a great opportunity to practice and learn more using Angular. My first project at Cobidol was to build a CACLTV Calculator, which I used Angular Materials for components and styling. After that, I was mainly working on building out reducers, effects, and selectors within the actual project while using Observables and saving data to the Store. I also had an opportunity to work on some tests using Jest Marble. These tests would allow us to create objects and check whether they had the correct properties attached to them.
+            I’ve been coding almost daily in my free time outside of work, focusing on ReactJS and Google Firebase. I believe during this time, I have become a more confident developer. I enjoy working in a team, especially with more senior developers, doing code reviews with me, allowing me to always learn from their experience. I believe that code is the future, I look around and everything we use today involves code one way or another. I want to be a part of that industry and to help it grow.
             <br />
-            My interest in web development and writing code was perked, as previously I had attempted to launch a tech startup doing vegan & vegetarian delivery service.
-            Having grown up enjoying building websites using online templates, being fascinated by both the programing behind it as well as a love for art and creativity, I decided to grow my skills in web development at General Assembly.
+            Previous to General Assembly, I had attempted to launch a tech startup doing vegan & vegetarian delivery service. This ran for a year and a half but unfortunately had to shelve this idea due to a lack of experience from both myself and my business partner. 
             <br />
-            Web development as a career excites me due to the scope of ability to move up within an organization. I also firmly believe that being able to learn and further those skills while working is a huge bonus that I have not seen in many industries before.
-            Ideally, I would be working within a close-knit, friendly development team, which would allow me to grow my skills over a long term basis. My dream company would be an ethical one that would help increase people's standard of living or health.
-            </p>
+            Web development as a career excites me due to the scope of ability to move up within an organization. I also firmly believe that being able to learn and further those skills while working is a huge bonus that I have not seen in many industries before. Ideally, I would be working within a close-knit, friendly development team, which would allow me to grow my skills over a long term basis. My dream company would be an ethical one that would help increase people's standard of living or health.            </p>
         </div>
     )
 }
 
 export default About;
+
+
+
+
