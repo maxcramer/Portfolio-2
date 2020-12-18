53d9299aeb6e645a88a9b5f5baad811c3c53e387
maxcramer <maximiliancramer@yahoo.com>
2020-12-18 16:20:59 +0000

    changing about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,11 +7,8 @@ function About() {
         <div id="about_container">
             <h3 id="about_title">About Max</h3>
             <p id="about_desc">
-            I’ve been coding almost daily in my free time outside of work, focusing on ReactJS and Google Firebase. I believe during this time, I have become a more confident developer. I enjoy working in a team, especially with more senior developers, doing code reviews with me, allowing me to always learn from their experience. I believe that code is the future, I look around and everything we use today involves code one way or another. I want to be a part of that industry and to help it grow.
-            <br />
-            Previous to General Assembly, I had attempted to launch a tech startup doing vegan & vegetarian delivery service. This ran for a year and a half but unfortunately had to shelve this idea due to a lack of experience from both myself and my business partner. 
-            <br />
-            Web development as a career excites me due to the scope of ability to move up within an organization. I also firmly believe that being able to learn and further those skills while working is a huge bonus that I have not seen in many industries before. Ideally, I would be working within a close-knit, friendly development team, which would allow me to grow my skills over a long term basis. My dream company would be an ethical one that would help increase people's standard of living or health.            </p>
+            I’ve been coding almost daily in my free time outside of work, focusing on ReactJS and Google Firebase. I believe during this time, I have become a more confident developer. I am currently attempting to grow my skills with a course on Udemy in ReactJS & React Native. I enjoy working in a team, especially with more senior developers, doing code reviews with me, allowing me to always learn from their experience. I believe that code is the future, I look around and everything we use today involves code one way or another. I want to be a part of that industry and to help it grow.
+            </p>
         </div>
     )
 }
