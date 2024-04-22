ed79035c55a7629bcd3994749ba5c40aeaf8ac89
maxcramer <maximiliancramer@yahoo.com>
2024-04-22 17:47:31 +0100

    kb page styled for mobile, restructured too

--- src/Pages/SabButter/SabButter.jsx
+++ src/Pages/SabButter/SabButter.jsx
@@ -61,55 +61,55 @@ const SabButter = () => {
             and growing his graphic design skills at the same time.
           </p>
         </div>
-        <div className="kb_example">
-          <h4 className="kb_example_title">
-            The Adventures of
-            <br /> Sabba & Butterscotch:
-          </h4>
-          <h4 className="kb_example_title_sub">Blue Gets a Booboo</h4>
-          <div className="kb_example_text">
-            <p>
-              There once was a boat called Blue, 
-              <br /> Who upon it lived an animal or two.
-            </p>
-            <p>
-              Sabba, a rather curious cat,  <br />
-              And her best friend butterscotch, a pig that’s quite fat! 
-            </p>
-            <p>
-              All day long they would go up and down the canals, <br />
-              All along the way, saying hi to their pals! 
-            </p>
-            <p>
-              But one day their boat got stuck, <br />  “Oh No” Meow’d Sabba,
-              but then saw a duck!
-            </p>
-            <p>
-              “They Call me Larry, Larry La Duck”, <br /> “It Looks like your
-              boat, blue has been struck!”
-            </p>
-            <p>
-              “Do you need some help there?” Quacked Larry,
-              <br />  “I can get my friends, Sally, Barry & Gary”
-            </p>
-            <p>
-              “Yes please” oinked Butterscotch, “we don’t know what to do!”,{" "}
-              <br />
-              Larry looked up and quacked “I think Blue has a booboo!”
-            </p>
-          </div>
-        </div>
-        <h4>
-          To see how Sabba, Butterscotch & Blue make it out of their adventure,
-          you will have to wait until the book has been published.
-          <br />
-          <br />
-          If you are interested and would like to discuss the series further,
-          please email{" "}
-          <a href="mailto:info@maxcramer101.com">info@maxcramer101.com</a> with
-          the subject "The Adventures of Sabba and Butterscotch".
-        </h4>
       </div>
+      <div className="kb_example">
+        <h4 className="kb_example_title">
+          The Adventures of
+          <br /> Sabba & Butterscotch:
+        </h4>
+        <h4 className="kb_example_title_sub">Blue Gets a Booboo</h4>
+        <div className="kb_example_text">
+          <p>
+            There once was a boat called Blue, 
+            <br /> Who upon it lived an animal or two.
+          </p>
+          <p>
+            Sabba, a rather curious cat,  <br />
+            And her best friend butterscotch, a pig that’s quite fat! 
+          </p>
+          <p>
+            All day long they would go up and down the canals, <br />
+            All along the way, saying hi to their pals! 
+          </p>
+          <p>
+            But one day their boat got stuck, <br />  “Oh No” Meow’d Sabba, but
+            then saw a duck!
+          </p>
+          <p>
+            “They Call me Larry, Larry La Duck”, <br /> “It Looks like your
+            boat, blue has been struck!”
+          </p>
+          <p>
+            “Do you need some help there?” Quacked Larry,
+            <br />  “I can get my friends, Sally, Barry & Gary”
+          </p>
+          <p>
+            “Yes please” oinked Butterscotch, “we don’t know what to do!”,{" "}
+            <br />
+            Larry looked up and quacked “I think Blue has a booboo!”
+          </p>
+        </div>
+      </div>
+      <h4>
+        To see how Sabba, Butterscotch & Blue make it out of their adventure,
+        you will have to wait until the book has been published.
+        <br />
+        <br />
+        If you are interested and would like to discuss the series further,
+        please email{" "}
+        <a href="mailto:info@maxcramer101.com">info@maxcramer101.com</a> with
+        the subject "The Adventures of Sabba and Butterscotch".
+      </h4>
     </div>
   );
 };
