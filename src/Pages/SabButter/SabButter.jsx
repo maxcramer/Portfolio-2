a82b4cfcad4e0f9af03b83a238191af583fcffd3
maxcramer <maximiliancramer@yahoo.com>
2024-04-22 15:49:57 +0100

    title banner for sabba and butter scotch page finished for desktop, to work on page content next

--- src/Pages/SabButter/SabButter.jsx
+++ src/Pages/SabButter/SabButter.jsx
@@ -1,9 +1,42 @@
 import React from "react";
 
+import wideshot from "../../components/media/wideshot.webp";
+
 import "./SabButter.scss";
 
 const SabButter = () => {
-  return <div>KIDS BOOK</div>;
+  return (
+    <div>
+      <div className="kb_title_container">
+        <h2>
+          The Adventures of <br></br>Sabba & Butterscotch
+        </h2>
+        <img src={wideshot} alt="Sabba & Buttescotch" />
+        <h6>Artwork generated on MidjourneyAi</h6>
+      </div>
+      <div className="kb_main">
+        <p>
+          ipsum Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Mollitia distinctio voluptas minus aliquam vero sunt quas, recusandae
+          esse! Assumenda minima commodi cum et ipsa praesentium voluptate eaque
+          fuga modi impedit! ipsum Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Mollitia distinctio voluptas minus aliquam vero sunt
+          quas, recusandae esse! Assumenda minima commodi cum et ipsa
+          praesentium voluptate eaque fuga modi impedit! ipsum Lorem ipsum dolor
+          sit, amet consectetur adipisicing elit. Mollitia distinctio voluptas
+          minus aliquam vero sunt quas, recusandae esse! Assumenda minima
+          commodi cum et ipsa praesentium voluptate eaque fuga modi impedit!
+          ipsum Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Mollitia distinctio voluptas minus aliquam vero sunt quas, recusandae
+          esse! Assumenda minima commodi cum et ipsa praesentium voluptate eaque
+          fuga modi impedit! ipsum Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Mollitia distinctio voluptas minus aliquam vero sunt
+          quas, recusandae esse! Assumenda minima commodi cum et ipsa
+          praesentium voluptate eaque fuga modi impedit!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SabButter;
