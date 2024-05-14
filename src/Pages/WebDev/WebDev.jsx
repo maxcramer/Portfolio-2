a7b8f84045cd734650b4b581c4a39358e93332a7
maxcramer <maximiliancramer@yahoo.com>
2024-05-14 14:47:31 +0100

    personal projects index designed for mobile

--- src/Pages/WebDev/WebDev.jsx
+++ src/Pages/WebDev/WebDev.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import PersonalProjects from "../../components/PersonalProjects/PersonalProjects";
+
 import own from "../../components/media/own.png";
 import owselectorlogo from "../../components/media/owselectorlogo.png";
 import FTH_Logo from "../../components/media/FTH_Logo.png";
@@ -14,57 +16,7 @@ const WebDev = () => {
       <div className="webdev_title">
         <h1>Web Development</h1>
       </div>
-      <h2>Personal Projects</h2>
-      <div className="pp_container">
-        <a href="/own" className="pp_item">
-          <div className="hidden_text">
-            <h3>Onewheel News</h3>
-            <p>Find out the latest news from the world of Onewheel.</p>
-          </div>
-          <div className="pp_logo">
-            <img src={own} alt="" />
-          </div>
-        </a>
-        <a href="/ow-selector-app" className="pp_item">
-          <div className="hidden_text">
-            <h3>Onewheel Selector App</h3>
-            <p>Find the best Onewheel that is right for you!</p>
-          </div>
-          <div className="pp_logo">
-            <img src={owselectorlogo} alt="" />
-          </div>
-        </a>
-        <a href="/fth" className="pp_item">
-          <div className="hidden_text">
-            <h3>Float The Highlands</h3>
-            <p>The UK's first ever Onewheel Competition (Launched 2023)</p>
-          </div>
-          <div className="pp_logo">
-            <img src={FTH_Logo} alt="" />
-          </div>
-        </a>
-        <a href="/is-biscuit" className="pp_item">
-          <div className="hidden_text">
-            <h3>Is It a Biscuit?</h3>
-            <p>Always wondered if it's a biscuit? Now you can check!</p>
-          </div>
-          <div className="pp_logo">
-            <img src={biscuit_logo} alt="" />
-          </div>
-        </a>
-        <a href="/insuremypev" className="pp_item">
-          <div className="hidden_text">
-            <h3>Insure My Pev</h3>
-            <p>
-              A landing page for an personal electric vehicle insurance
-              business.
-            </p>
-          </div>
-          <div className="pp_logo">
-            <img src={logo} alt="" />
-          </div>
-        </a>
-      </div>
+      <PersonalProjects />
     </div>
   );
 };
--- src/components/PersonalProjects/PersonalProjects.jsx
+++ src/components/PersonalProjects/PersonalProjects.jsx
@@ -1,60 +1,134 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
 
-import { getPersonalProjects } from '../../services/firestoreService';
-import GitHubLogo from '../media/githubLogo.png';
-import LinkLogo from '../media/link-2.png';
+import own from "../media/own.png";
+import owselectorlogo from "../media/owselectorlogo.png";
+import FTH_Logo from "../media/FTH_Logo.png";
+import biscuit_logo from "..//media/biscuit_logo.png";
+import logo from "../media/insuremypev.png";
 
-import './PersonalProjects.scss';
+import "./PersonalProjects.scss";
 
-function PersonalProjects() {
-    const [personalProjects, setPersonalProjects] = useState([]);
-
-    useEffect(() => {
-        async function fetchData() {
-            const personalProjects = await getPersonalProjects();
-            setPersonalProjects(personalProjects);
-        }
-        fetchData();
-    }, [])
-    return (
-        <div className="personalProjects_container" id="personalprojects">
-            <h2 id="pp_title">Personal Side Projects</h2>
-            <ul>
-                {personalProjects.map(p => (
-                    <div key={p._id} className="personal_projects_list_item">
-                        <li className="list_item">
-                            <img className="main_image" src={p.image} alt=""/>
-                            <div className="info_div">
-                                <h3>
-                                    {p.title}
-                                </h3>
-                                <div className="links">
-                                <a href={p.github}>
-                                    <img src={GitHubLogo} alt="" className="github_logo"/>
-                                </a>
-                                <a href={p.url}>
-                                    <div className="url_container">
-                                        <img src={LinkLogo} alt="" className="url" />
-                                    </div>                
-                                </a>
-                                </div>
-                            </div>
-                            <div>
-                                <p className="proj_status">Status: {p.status}</p>
-                            </div>
-                            <div>
-                                <div className="stack">
-                                    Stack: {p.stack}
-                                </div>
-                            </div>
-
-                            <p className="desc">{p.desc}</p>
-                        </li>
-                    </div>
-                ))}
-            </ul>
+const PersonalProjects = () => {
+  return (
+    <div className="pp_parent_cont">
+      <h2>Personal Projects</h2>
+      <div className="pp_container">
+        <a href="/own" className="pp_item">
+          <div className="hidden_text">
+            <h3>Onewheel News</h3>
+            <p>Find out the latest news from the world of Onewheel.</p>
+          </div>
+          <div className="pp_logo">
+            <img src={own} alt="" />
+          </div>
+        </a>
+        <a href="/ow-selector-app" className="pp_item">
+          <div className="hidden_text">
+            <h3>Onewheel Selector App</h3>
+            <p>Find the best Onewheel that is right for you!</p>
+          </div>
+          <div className="pp_logo">
+            <img src={owselectorlogo} alt="" />
+          </div>
+        </a>
+        <a href="/fth" className="pp_item">
+          <div className="hidden_text">
+            <h3>Float The Highlands</h3>
+            <p>The UK's first ever Onewheel Competition (Launched 2023)</p>
+          </div>
+          <div className="pp_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </a>
+        <a href="/is-biscuit" className="pp_item">
+          <div className="hidden_text">
+            <h3>Is It a Biscuit?</h3>
+            <p>Always wondered if it's a biscuit? Now you can check!</p>
+          </div>
+          <div className="pp_logo">
+            <img src={biscuit_logo} alt="" />
+          </div>
+        </a>
+        <a href="/insuremypev" className="pp_item">
+          <div className="hidden_text">
+            <h3>Insure My Pev</h3>
+            <p>
+              A landing page for an personal electric vehicle insurance
+              business.
+            </p>
+          </div>
+          <div className="pp_logo">
+            <img src={logo} alt="" />
+          </div>
+        </a>
+      </div>
+      <div className="pp_container mobile">
+        <div className="pp_item">
+          <div className="hidden_text">
+            <h3>Onewheel News</h3>
+            <p>Find out the latest news from the world of Onewheel.</p>
+            <p>
+              <a href="/own">Click here</a> for more.
+            </p>
+          </div>
+          <div className="pp_logo">
+            <img src={own} alt="" />
+          </div>
         </div>
-    )
-}
+        <div className="pp_item">
+          <div className="hidden_text">
+            <h3>Onewheel Selector App</h3>
+            <p>Find the best Onewheel that is right for you!</p>
+            <p>
+              <a href="/ow-selector-app">Click here</a> for more.
+            </p>
+          </div>
+          <div className="pp_logo">
+            <img src={owselectorlogo} alt="" />
+          </div>
+        </div>
+        <div className="pp_item">
+          <div className="hidden_text">
+            <h3>Float The Highlands</h3>
+            <p>The UK's first ever Onewheel Competition (Launched 2023)</p>
+            <p>
+              <a href="/fth">Click here</a> for more.
+            </p>
+          </div>
+          <div className="pp_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </div>
+        <div className="pp_item">
+          <div className="hidden_text">
+            <h3>Is It a Biscuit?</h3>
+            <p>Always wondered if it's a biscuit? Now you can check!</p>
+            <p>
+              <a href="/is-biscuit">Click here</a> for more.
+            </p>
+          </div>
+          <div className="pp_logo">
+            <img src={biscuit_logo} alt="" />
+          </div>
+        </div>
+        <div className="pp_item">
+          <div className="hidden_text">
+            <h3>Insure My Pev</h3>
+            <p>
+              A landing page for an personal electric vehicle insurance
+              business.
+            </p>
+            <p>
+              <a href="/insuremypev">Click here</a> for more.
+            </p>
+          </div>
+          <div className="pp_logo">
+            <img src={logo} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default PersonalProjects;
