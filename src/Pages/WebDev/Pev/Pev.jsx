c51b3616d6b80afe1d71b35fdd4db16a34fed0d1
maxcramer <maximiliancramer@yahoo.com>
2024-05-21 12:17:36 +0100

    finished styling for desktop insure pev page

--- src/Pages/WebDev/Pev/Pev.jsx
+++ src/Pages/WebDev/Pev/Pev.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 import link from "../../../components/media/link-2.png";
-
+import pev_desktop from "../../../components/media/insure_pev_dekstop.png";
+import pev_mobile from "../../../components/media/insure_pev_mobile.png";
 import "./Pev.scss";
 
 const Pev = () => {
@@ -18,7 +19,7 @@ const Pev = () => {
           </a>
         </div>
       </div>
-      <div id="pev" className="pev_info">
+      <div id="pev" className="pev_info_container">
         <div className="pev_title_container">
           <h1>Insure My Pev</h1>
           <div className="pev_subtitle">
@@ -27,6 +28,48 @@ const Pev = () => {
             <a href="https://insuremypev.com/" target="#blank">
               <img src={link} alt="" />
             </a>
+          </div>
+        </div>
+        <div className="pev_info">
+          <div className="pev_desktop">
+            <p>Desktop View</p>
+            <img src={pev_desktop} alt="" />
+          </div>
+          <div className="pev_mobile">
+            <p>Mobile View</p>
+            <img src={pev_mobile} alt="" />
+          </div>
+          <div className="pev_text">
+            <p>
+              ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Rerum labore sint doloremque excepturi adipisci assumenda aliquid
+              repellat neque illum officia quos est aspernatur deleniti sed
+              possimus in ad, unde nisi? ipsum Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Rerum labore sint doloremque
+              excepturi adipisci assumenda aliquid repellat neque illum officia
+              quos est aspernatur deleniti sed possimus in ad, unde nisi? ipsum
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+              labore sint doloremque excepturi adipisci assumenda aliquid
+              repellat neque illum officia quos est aspernatur deleniti sed
+              possimus in ad, unde nisi? ipsum Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Rerum labore sint doloremque
+              excepturi adipisci assumenda aliquid repellat neque illum officia
+              quos est aspernatur deleniti sed possimus in ad, unde nisi? ipsum
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+              labore sint doloremque excepturi adipisci assumenda aliquid
+              repellat neque illum officia quos est aspernatur deleniti sed
+              possimus in ad, unde nisi? ipsum Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Rerum labore sint doloremque
+              excepturi adipisci assumenda aliquid repellat neque illum officia
+              quos est aspernatur deleniti sed possimus in ad, unde nisi? ipsum
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+              labore sint doloremque excepturi adipisci assumenda aliquid
+              repellat neque illum officia quos est aspernatur deleniti sed
+              possimus in ad, unde nisi? ipsum Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Rerum labore sint doloremque
+              excepturi adipisci assumenda aliquid repellat neque illum officia
+              quos est aspernatur deleniti sed possimus in ad, unde nisi?
+            </p>
           </div>
         </div>
       </div>
