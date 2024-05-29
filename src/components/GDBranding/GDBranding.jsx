3eb9de24a71012301dc566e45defe56e845fac24
maxcramer <maximiliancramer@yahoo.com>
2024-05-29 17:10:54 +0100

    updating branding and print components for graphic design index page

--- src/components/GDBranding/GDBranding.jsx
+++ src/components/GDBranding/GDBranding.jsx
@@ -1,12 +1,209 @@
 import React from "react";
 
+import FTH_Logo from "../media/FTH_Logo.png";
+
+import bespokemtb from "../media/BespokeMBLogoTests-06.png";
+import bodywash from "../media/Body Wash 3D.png";
+import MDZynes from "../media/MD-Zynes.jpg";
+import buscardslogo from "../media/Business Cards Logo-05.png";
+import sodacans from "../media/cans-snapshot.png";
+import pacificcalm from "../media/PacificCalmLogo.png";
+import racebib from "../media/RaceBib.png";
+import videoflux from "../media/Asset 4@3x.png";
 import "./GDBranding.scss";
 
 const GDBranding = () => {
   return (
     <div className="branding_container">
       <h2>Branding</h2>
-      <div className="branding_items_parent"></div>
+      <div className="branding_items_parent">
+        <a href="/bespokemtb" className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>Bespoke Mountain Bikes</h3>
+            <p>Logo Design for Mountain Bikes Business</p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={bespokemtb} alt="" />
+          </div>
+        </a>
+        <a href="/mlc" className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>Malluable Life Coaching</h3>
+            <p>Brand design for a life coaching business</p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={buscardslogo} alt="" />
+          </div>
+        </a>
+        <a href="/pacificcalm" className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>Pacific Calm Logo</h3>
+            <p>A Logo for a Health & wellness Spa.</p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={pacificcalm} alt="" />
+          </div>
+        </a>
+        <a href="/mdzynes" className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>MD Zynes</h3>
+            <p>Branding for Graphic Design Business</p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={MDZynes} alt="" />
+          </div>
+        </a>
+        <a href="/insuremypev" className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>DynaMix Packaging Design</h3>
+            <p>Package design for a soda can.</p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={sodacans} alt="" />
+          </div>
+        </a>
+        <a href="/ow-selector-app" className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>Body Wash Label</h3>
+            <p>Designing a Label for a Body Wash Brand</p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={bodywash} alt="" />
+          </div>
+        </a>
+        <a href="/insuremypev" className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>VideoFlux</h3>
+            <p>A Logo for a new online streaming platform</p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={videoflux} alt="" />
+          </div>
+        </a>
+        {/* <a href="/insuremypev" className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>Shred The Shires Race Numbers</h3>
+            <p>
+              Design for Race Bibs and Numbers for Shred The Shires Onewheel
+              Festival
+            </p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={racebib} alt="" />
+          </div>
+        </a> */}
+      </div>
+      <div className="branding_items_parent mobile">
+        <div className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>Bespoke Mountain Bikes</h3>
+            <p>Logo Design for Mountain Bikes Business</p>
+            <p>
+              <a href="/bespokemtb">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={bespokemtb} alt="" />
+          </div>
+        </div>
+        <div className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>Onewheel Selector App</h3>
+            <p>Find the best Onewheel that is right for you!</p>
+            <p>
+              <a href="/ow-selector-app">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </div>
+        <div className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>Float The Highlands</h3>
+            <p>The UK's first ever Onewheel Competition (Launched 2023)</p>
+            <p>
+              <a href="/fth">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </div>
+        <div className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>Is It a Biscuit?</h3>
+            <p>Always wondered if it's a biscuit? Now you can check!</p>
+            <p>
+              <a href="/is-biscuit">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </div>
+        <div className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>Insure My Pev</h3>
+            <p>
+              A landing page for an personal electric vehicle insurance
+              business.
+            </p>
+            <p>
+              <a href="/insuremypev">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </div>
+
+        <div className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>Insure My Pev</h3>
+            <p>
+              A landing page for an personal electric vehicle insurance
+              business.
+            </p>
+            <p>
+              <a href="/insuremypev">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </div>
+        <div className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>Insure My Pev</h3>
+            <p>
+              A landing page for an personal electric vehicle insurance
+              business.
+            </p>
+            <p>
+              <a href="/insuremypev">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </div>
+        <div className="GDBranding_item">
+          <div className="hidden_text">
+            <h3>Insure My Pev</h3>
+            <p>
+              A landing page for an personal electric vehicle insurance
+              business.
+            </p>
+            <p>
+              <a href="/insuremypev">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDBranding_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/GDPrint/GDPrint.jsx
+++ src/components/GDPrint/GDPrint.jsx
@@ -1,13 +1,204 @@
 import React from "react";
 
+import FTH_Logo from "../media/FTH_Logo.png";
+import bobs from "../media/FINAL POSTER.png";
+import bookcover from "../media/Book Cover Final boths sides 2.png";
+import poster from "../media/Moden FINAL.png";
+import journeyposter from "../media/Journey movie Poster.png";
+import mag from "../media/MagSpred1.png";
+import racebib from "../media/RaceBib.png";
+import cookbook from "../media/Cook Book-Digital.png";
+
 import "./GDPrint.scss";
 const GDPrint = () => {
   return (
     <div className="print_container">
       <h2>Print</h2>
-      <div className="print_items_parent"></div>
+      <div className="print_items_parent">
+        {" "}
+        <a href="/bbmovie" className="GDPrint_item">
+          <div className="hidden_text">
+            <h3>Bob's Burgers: The Movie - Poster</h3>
+            <p>A Poster Design for Bob's Burgers: The Movie</p>
+          </div>
+          <div className="GDPrint_logo">
+            <img src={bobs} alt="" />
+          </div>
+        </a>
+        <a href="/pacificcalm" className="GDPrint_item">
+          <div className="hidden_text">
+            <h3>Night Club Poster</h3>
+            <p>A Promotional Poster for a Nightclub Event</p>
+          </div>
+          <div className="GDPrint_logo">
+            <img src={poster} alt="" />
+          </div>
+        </a>
+        <a href="/mdzynes" className="GDPrint_item">
+          <div className="hidden_text">
+            <h3>Journey: A Onewheel Movie</h3>
+            <p>A Poster Designed for Journey: A Onewheel Movie</p>
+          </div>
+          <div className="GDPrint_logo">
+            <img src={journeyposter} alt="" />
+          </div>
+        </a>
+        <a href="/mlc" className="GDPrint_item">
+          <div className="hidden_text">
+            <h3>The Garden of Secrets</h3>
+            <p>Book Cover Design for a book about Gardening</p>
+          </div>
+          <div className="GDPrint_logo">
+            <img src={bookcover} alt="" />
+          </div>
+        </a>
+        <a href="/mlc" className="GDPrint_item">
+          <div className="hidden_text">
+            <h3>Magazine Spread</h3>
+            <p>Designing Layout for Double Page Magazine Spread</p>
+          </div>
+          <div className="GDPrint_logo">
+            <img src={mag} alt="" />
+          </div>
+        </a>
+        <a href="/mlc" className="GDPrint_item">
+          <div className="hidden_text">
+            <h3>Shred The Shires Race Numbers</h3>
+            <p>
+              Design for Race Bibs and Numbers for Shred The Shires Onewheel
+              Festival
+            </p>
+          </div>
+          <div className="GDPrint_logo">
+            <img src={racebib} alt="" />
+          </div>
+        </a>
+        <a href="/mlc" className="GDPrint_item">
+          <div className="hidden_text">
+            <h3>Cook Book Design & Layout</h3>
+            <p>
+              A Dessert Cook Book fully Designed and laid out ready for printing
+            </p>
+          </div>
+          <div className="GDPrint_logo">
+            <img src={cookbook} alt="" />
+          </div>
+        </a>
+      </div>
+      <div className="branding_items_parent mobile">
+        <div className="GDPrint_item">
+          <div className="hidden_text">
+            <h3>Bespoke Mountain Bikes</h3>
+            <p>Logo Design for Mountain Bikes Business</p>
+            <p>
+              <a href="/bespokemtb">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDPrint_logo">
+            <img src={bobs} alt="" />
+          </div>
+        </div>
+        <div className="GDPrint_item">
+          <div className="hidden_text">
+            <h3>Onewheel Selector App</h3>
+            <p>Find the best Onewheel that is right for you!</p>
+            <p>
+              <a href="/ow-selector-app">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDPrint_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </div>
+        <div className="GDPrint_item">
+          <div className="hidden_text">
+            <h3>Float The Highlands</h3>
+            <p>The UK's first ever Onewheel Competition (Launched 2023)</p>
+            <p>
+              <a href="/fth">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDPrint_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </div>
+        <div className="GDPrint_item">
+          <div className="hidden_text">
+            <h3>Is It a Biscuit?</h3>
+            <p>Always wondered if it's a biscuit? Now you can check!</p>
+            <p>
+              <a href="/is-biscuit">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDPrint_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </div>
+        <div className="GDPrint_item">
+          <div className="hidden_text">
+            <h3>Insure My Pev</h3>
+            <p>
+              A landing page for an personal electric vehicle insurance
+              business.
+            </p>
+            <p>
+              <a href="/insuremypev">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDPrint_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </div>
+
+        <div className="GDPrint_item">
+          <div className="hidden_text">
+            <h3>Insure My Pev</h3>
+            <p>
+              A landing page for an personal electric vehicle insurance
+              business.
+            </p>
+            <p>
+              <a href="/insuremypev">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDPrint_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </div>
+        <div className="GDPrint_item">
+          <div className="hidden_text">
+            <h3>Insure My Pev</h3>
+            <p>
+              A landing page for an personal electric vehicle insurance
+              business.
+            </p>
+            <p>
+              <a href="/insuremypev">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDPrint_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </div>
+        <div className="GDPrint_item">
+          <div className="hidden_text">
+            <h3>Insure My Pev</h3>
+            <p>
+              A landing page for an personal electric vehicle insurance
+              business.
+            </p>
+            <p>
+              <a href="/insuremypev">Click here</a> for more.
+            </p>
+          </div>
+          <div className="GDPrint_logo">
+            <img src={FTH_Logo} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default GDPrint;
+//
