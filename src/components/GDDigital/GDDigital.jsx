bfe4ebba602cef1a7fc0d595347d2511af686dd3
maxcramer <maximiliancramer@yahoo.com>
2024-05-30 10:30:42 +0100

    adding content to digital section no gd page and index, all artwork indexed

--- src/components/GDDigital/GDDigital.jsx
+++ src/components/GDDigital/GDDigital.jsx
@@ -1,13 +1,62 @@
-import React from 'react'
+import React from "react";
 
-import './GDDigital.scss';
+import YTCans from "../media/CansThumbnail.png";
+import YTTravel from "../media/TravelThumbnail.png";
+
+import "./GDDigital.scss";
 const GDDigital = () => {
   return (
-    <div className='digital_container'>
+    <div className="digital_container">
       <h2>Digital</h2>
-      <div className="digital_items_parent"></div>
+      <div className="digital_items_parent">
+        {" "}
+        <a href="/bbmovie" className="GDDig_item">
+          <div className="hidden_text">
+            <h3>Seltzer Taste Test</h3>
+            <p>A Youtube Thumbnail for a Video Reviewing Seltzers</p>
+          </div>
+          <div className="GDDig_logo">
+            <img src={YTCans} alt="" />
+          </div>
+        </a>
+        <a href="/pacificcalm" className="GDDig_item">
+          <div className="hidden_text">
+            <h3>Travel Hacks</h3>
+            <p>A Youtube Thumbnail for a Travel Hacks Video </p>
+          </div>
+          <div className="GDDig_logo">
+            <img src={YTTravel} alt="" />
+          </div>
+        </a>
+      </div>
+      <div className="digital_items_parent mobile">
+        <div className="GDDig_item">
+          <div className="hidden_text">
+            <h3>Seltzer Taste Test</h3>
+            <p>A Youtube Thumbnail for a Video Reviewing Seltzers</p>
+            <p>
+              <a href="/bespokemtb">Tap here</a> for more.
+            </p>
+          </div>
+          <div className="GDDig_logo">
+            <img src={YTCans} alt="" />
+          </div>
+        </div>
+        <div className="GDDig_item">
+          <div className="hidden_text">
+            <h3>Travel Hacks</h3>
+            <p>A Youtube Thumbnail for a Travel Hacks Video </p>
+            <p>
+              <a href="/ow-selector-app">Tap here</a> for more.
+            </p>
+          </div>
+          <div className="GDDig_logo">
+            <img src={YTTravel} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GDDigital
+export default GDDigital;
