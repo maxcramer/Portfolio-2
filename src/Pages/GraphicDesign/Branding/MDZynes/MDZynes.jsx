54c144c54fd9a44dd0055ba9a955cb31216ee026
maxcramer <maximiliancramer@yahoo.com>
2024-06-04 16:44:36 +0100

    finished desgning MDZynes page for desktop

--- src/Pages/GraphicDesign/Branding/MDZynes/MDZynes.jsx
+++ src/Pages/GraphicDesign/Branding/MDZynes/MDZynes.jsx
@@ -2,6 +2,16 @@ import React from "react";
 
 import MDLogo from "../../../../components/media/MD-ZynesLogo.png";
 import MDSketches from "../../../../components/media/MdesignsLogoMockups-00.jpg";
+import MDIll03 from "../../../../components/media/MdesignsLogoMockups-03.png";
+import MDIll04 from "../../../../components/media/MdesignsLogoMockups-04.png";
+import MDIll05 from "../../../../components/media/MdesignsLogoMockups-05.png";
+import MDLH from "../../../../components/media/MDLetterHead.jpg";
+import MDS from "../../../../components/media/MDStationary.png";
+import MDBCF from "../../../../components/media/MDBusCardFront.png";
+import MDBCB from "../../../../components/media/MDBusCardBack.png";
+import MDFCF from '../../../../components/media/MDSnapback1.png';
+import MDFCS from "../../../../components/media/MDSnapback2.png";
+import MDPC from '../../../../components/media/MDPhoneCase.png';
 
 import "./MDZynes.scss";
 
@@ -59,7 +69,7 @@ const MDZynes = () => {
             </div>
           </div>
           <div className="md_sketch">
-            <h3>Title</h3>
+            <h3>Sketching out initial ideas</h3>
             <p>
               ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Illum, porro dolores eum aliquid consequuntur inventore, eius
@@ -68,6 +78,73 @@ const MDZynes = () => {
               sit amet consectetur adipisicing elit. Illum, porro dolores eum
             </p>
             <img src={MDSketches} alt="" />
+          </div>
+        </div>
+        <div className="md_process">
+          <img src={MDIll03} alt="" />
+          <div>
+            <h3>Testing Ideas in Illustrator</h3>
+            <p>
+              Ipsum Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Non natus deserunt asperiores, amet ab distinctio accusamus et
+              odit commodi eum minima quas aliquam, ratione error molestias
+              quisquam cumque earum dicta. Ipsum Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Non natus deserunt asperiores, amet
+              ab distinctio accusamus et odit commodi eum minima quas aliquam,
+              ratione error molestias quisquam cumque earum dicta. Ipsum Lorem
+              ipsum dolor sit amet consectetur, adipisicing elit. Non natus
+              deserunt asperiores, amet ab distinctio accusamus et odit commodi
+              eum minima quas aliquam, ratione error molestias quisquam cumque
+              earum dicta.
+            </p>
+          </div>
+        </div>
+        <div className="md_process_cont">
+          <div>
+            <p></p>
+            <img src={MDIll04} alt="" />
+          </div>
+          <div>
+            <p></p>
+            <img src={MDIll05} alt="" />
+          </div>
+        </div>
+        <div className="md_forClient">
+          <h3>Office Supply Mockups</h3>
+          <div className="md_forClient_imgs">
+            <div>
+              <p>Final Logo</p>
+              <img src={MDBCF} alt="" />
+            </div>
+            <div>
+              <p>Final Logo</p>
+              <img src={MDBCB} alt="" />
+            </div>
+            <div>
+              <p>Final Logo</p>
+              <img src={MDS} alt="" />
+            </div>
+            <div>
+              <p>Final Logo</p>
+              <img src={MDLH} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="md_forClient">
+          <h3>Product Mockup</h3>
+          <div className="md_forClient_imgs">
+            <div>
+              <p>Final Logo</p>
+              <img src={MDFCF} alt="" />
+            </div>
+            <div>
+              <p>Final Logo</p>
+              <img src={MDFCS} alt="" />
+            </div>
+            <div>
+              <p>Final Logo</p>
+              <img src={MDPC} alt="" />
+            </div>
           </div>
         </div>
       </div>
