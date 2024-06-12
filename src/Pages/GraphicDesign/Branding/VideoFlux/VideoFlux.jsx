c0ad8ca6ca2e4bed98bc925efa4d86cbe6e8303b
maxcramer <maximiliancramer@yahoo.com>
2024-06-12 15:30:25 +0100

    description written for vid flux page! well done bro we getting there, glad to see your still watching

--- src/Pages/GraphicDesign/Branding/VideoFlux/VideoFlux.jsx
+++ src/Pages/GraphicDesign/Branding/VideoFlux/VideoFlux.jsx
@@ -64,17 +64,6 @@ const VideoFlux = () => {
               </p>
             </div>
           </div>
-          <div>
-            <h3>Client wishes:</h3>
-            <div>
-              <p>Emphasis on reduce stress service</p>
-              <p>Does not want masculine qualities to logo</p>
-              <p>Does not want to be seen as just a spa</p>
-              <p>They have a pool & sauna area</p>
-              <p>A lot of water based services: spa & exercise</p>
-              <p>Want to work in the spas location to logo & brand</p>
-            </div>
-          </div>
         </div>
         <div className="vf_process">
           <div className="vf_process_firstRow">
@@ -84,14 +73,28 @@ const VideoFlux = () => {
             <div>
               <h3>Testing Fonts in Illustrator</h3>
               <p>
-                Ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Sed tempora voluptates laudantium id doloribus, voluptate
-                asperiores eius provident quia animi minus enim. Vero commodi
-                voluptatum distinctio at saepe doloribus quam? Ipsum Lorem ipsum
-                dolor sit amet consectetur adipisicing elit. Sed tempora
-                voluptates laudantium id doloribus, voluptate asperiores eius
-                provident quia animi minus enim. Vero commodi voluptatum
-                distinctio at saepe doloribus quam?
+                This logo needed to be readable in small spaces such as headers
+                of a mobile app, and were looking for the main design to be of
+                the text. With that in mind, I started this project by finding
+                fonts that I thought might work.
+                <br />I then selected the two I thought to be most appropriate,
+                Modak and Impact.
+                <br />
+                Initially with Modak, I was trying to make the O use whitespace
+                to create the shape and then have a lense in the middle, to
+                represent the video recording aspect of the app. Over a few
+                iterations, this became a play button, which felt a lot more on
+                trend for an app such as this.
+                <br />
+                Next I started experimenting with the Impact font, attempting to
+                create known video associated icons out of the lettering. This
+                had some success but felt very messy and thought it would be
+                hard to read from a distance with so much detailing.
+                <br />I then took my favorite designs and laid them out to see
+                which was the best suiting. I eventually combined the two, using
+                the Impact font, while taking the play button on the O that I
+                had initially designed when using the Modak font. This gave a
+                bold, readable font, with related branding to the product.
               </p>
             </div>
           </div>
