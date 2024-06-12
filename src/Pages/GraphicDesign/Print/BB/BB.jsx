42269b956357e1dc18e75d644a5bc94c4c058a23
maxcramer <maximiliancramer@yahoo.com>
2024-06-12 16:43:34 +0100

    description for bobs burgers page done

--- src/Pages/GraphicDesign/Print/BB/BB.jsx
+++ src/Pages/GraphicDesign/Print/BB/BB.jsx
@@ -18,17 +18,26 @@ const BB = () => {
         <div className="bb_process">
           <h3>Process</h3>
           <p>
-            ipsum Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Saepe, sit sed repellendus quia id magni quaerat, mollitia, ut
-            similique dignissimos ullam voluptatem impedit doloremque quidem
-            veniam? Nesciunt, odit. Excepturi, nam? ipsum Lorem, ipsum dolor sit
-            amet consectetur adipisicing elit. Saepe, sit sed repellendus quia
-            id magni quaerat, mollitia, ut similique dignissimos ullam
-            voluptatem impedit doloremque quidem veniam? Nesciunt, odit.
-            Excepturi, nam? ipsum Lorem, ipsum dolor sit amet consectetur
-            adipisicing elit. Saepe, sit sed repellendus quia id magni quaerat,
-            mollitia, ut similique dignissimos ullam voluptatem impedit
-            doloremque quidem veniam? Nesciunt, odit. Excepturi, nam?
+            I set myself the challenge to design a teaser film poster in
+            Illustrator. At the time I was watching the Bob's Burgers series and
+            decided to attempt to do one for that.
+            <br />I found some images online to use as references to the
+            animation style. I found the Bob's Burgers font online, as felt this
+            would be the only font that would work for this poster.
+            <br />
+            After finishing the design, I remembered that there was a Bob's
+            Burgers: The Movie, so I quickly had a google and found that they
+            had actually made a poster very similar to what I had done as a
+            teaser poster. I actually found this very positive as it ensured me
+            that during my design I was thinking along the right path for this
+            project.
+            <br />
+            There are a few parts of this design I would like to talk about.
+            Firstly is the icon of Bob at the bottom of the burger. I designed
+            this in illustrator and really enjoyed creating the character. The
+            next aspect, and this may seem a small detail, but, the grease lines
+            on the burger made such a difference compared to before, giving the
+            burger a real look of texture and depth.
           </p>
           <div className="bb_process_img">
             <p>Process & Reference Images</p>
